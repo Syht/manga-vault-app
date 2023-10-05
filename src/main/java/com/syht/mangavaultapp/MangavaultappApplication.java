9277501ae64132b45530a261212f8251e949121c
Thys Giaccone <dofawaza@hotmail.fr>
2023-10-05 18:29:57 +0200

    init Spring boot

---
+++ src/main/java/com/syht/mangavaultapp/MangavaultappApplication.java
@@ -0,0 +1,13 @@
+package com.syht.mangavaultapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangavaultappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangavaultappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syht/mangavaultapp/MangavaultappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syht.mangavaultapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangavaultappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
